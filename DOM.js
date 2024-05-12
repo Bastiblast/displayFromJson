c14170da5173ef41de093edfd66232a70f830d66
bastuce <million.bastien@gmail.com>
2024-05-12 14:01:28 +0200

    Néttoyage du code, JSdoc et ajustement des fonctions.

--- DOM.js
+++ DOM.js
@@ -1,4 +1,38 @@
-/**
+/** Ajout de Event sur le choix du type PlaceHolders à utiliser.
+ *  
+ */ 
+document.querySelectorAll(".dropdown-item").forEach(
+    
+    itemButton => {
+
+        itemButton.addEventListener("click",
+
+            async function(e){
+            const clickSelection = e.target.textContent.toLowerCase()
+
+            const json = await loadData(clickSelection,"freshJson")
+
+            cleanAndFillContainer(createHTMLElementByJson(json),"table-receiver")
+            }
+        )
+    }
+)
+
+/** Add Event listener on search input.
+ * 
+ */
+const searchInput = document.getElementById("search-input")
+
+searchInput.addEventListener("input",
+
+    async function(e){
+    const input = e.target.value
+    json = await sortTable(input,"freshJson")
+    cleanAndFillContainer(createHTMLElementByJson(json),"table-receiver")
+    }
+)
+
+/** Create thead from a json in html format.
  * 
  * @param {object} json 
  * @returns {string} HTML Header in string
@@ -7,42 +41,47 @@ function createHeaderFromJson(json){
     const headers = []
 
     if (json.length === undefined){
-   for (const head in json){
+        for (const head in json){
         headers.push(head)
-   }
-    } else {
+        }
+    } 
+    else {
         for (const head in json[0]){
             headers.push(head)
-       }
+        }
     }
-   const HTMLHeaders = headers.map((head) => `<th>${head}</th>` ).join("")
+
+const HTMLHeaders = headers.map((head) => `<th>${head}</th>` ).join("")
+
 return HTMLHeaders
 }
 
-/**
+/** Create tbody from a json in html format.
  * 
- * @param {*} json 
+ * @param {json} json 
  * @returns {string} HTML Body in string
  */
 function createBodyDataFromJson(json){
-let HTMLAllBody = ""
 
+    let HTMLAllBody = ""
 
     for (const body of json){
         let HTMLTdBody = ""
         let HTMLTrBody = ""
         const TrBody = []
+
         for (const td in body){
             TrBody.push(body[td])
-    HTMLTdBody = TrBody.map((td) => {
-        const stringifyTdObject = JSON.stringify(td).replaceAll("{","").replaceAll("}","").replaceAll('"',"").replaceAll(",","<br>")
-    if(typeof td !== "object"){return `<td>${td}</td>`}
-    else {return `<td>${stringifyTdObject}</td>`}}).join("")
-
+            HTMLTdBody = TrBody.map((td) => {
+            const stringifyTdObject = JSON.stringify(td).replaceAll("{","").replaceAll("}","").replaceAll('"',"").replaceAll(",","<br>")
+            if(typeof td !== "object"){return `<td>${td}</td>`}
+        else {return `<td>${stringifyTdObject}</td>`}}).join("")
         }
+
         HTMLTrBody = `<tr>${HTMLTdBody}<tr>`
         HTMLAllBody += HTMLTrBody
-        };
+    }
+
     return HTMLAllBody
 }
 
@@ -52,59 +91,34 @@ let HTMLAllBody = ""
  * @returns {Element} Return a div table of the given json.
  */
 function createHTMLElementByJson(json) {
+
     const newDiv = document.createElement("div")
     newDiv.classList = "table-container"
     newDiv.id = "result-table"
+
     const HTMLHeaders = createHeaderFromJson(json)
     const HTMLBodyData = createBodyDataFromJson(json)
 
-   //console.log("json[0]",json[0])
-  // console.log("json.length",json.length)
-   newDiv.innerHTML = `
-   <table id="table-view" class="table table-sm table-striped"><thead>
-   ${HTMLHeaders}</thead>
-   <tbody>
-   ${HTMLBodyData}
-   </tbody></table>
-   `
-    const tableReceiver = document.getElementById("table-receiver")
-
-    console.log("tableReceiver",tableReceiver)
-    console.log("newDiv",newDiv.innerHTML)
-    //tableReceiver.appendChild(newDiv)
+    newDiv.innerHTML = `
+    <table id="table-view" class="table table-sm table-striped"><thead>
+    ${HTMLHeaders}</thead>
+    <tbody>
+    ${HTMLBodyData}
+    </tbody></table>
+    `
+    
     return newDiv
 }
 
-// Ajout de Event sur le choix du type PlaceHolders à utiliser.
+/** Take string Input and Local Json name to filter the second by the first.
+ * 
+ * @param {string} input 
+ * @param {string} localJsonName 
+ * @returns 
+ */
+ async function sortTable(input,localJsonName){
 
- document.querySelectorAll(".dropdown-item").forEach(itemButton => {
-
-    itemButton.addEventListener("click",
-    async function(e){
-        const clickSelection = e.target.textContent.toLowerCase()
-
-const json = await loadData(clickSelection)
-
-//console.log("loadData(clickSelection) = ",loadData(clickSelection))
-console.log("json = ",json)
-loadSelection(createHTMLElementByJson(json))
-
-}
-)
-})
-
-const searchInput = document.getElementById("search-input")
-
-searchInput.addEventListener("input",async function(e){
-    const input = e.target.value
-    console.log("input",input)
-    json = await sortTable(input)
-    loadSelection(createHTMLElementByJson(json))
-})
-
- async function sortTable(input){
-
-  const stringJson = await localStorage.getItem("freshJson")
+  const stringJson = await localStorage.getItem(localJsonName)
 
     const json =  await JSON.parse(stringJson)
 
@@ -119,8 +133,10 @@ searchInput.addEventListener("input",async function(e){
       let isTrue = false
       for (let eachCol in fjson){
       //  console.log("fjson[eachCol] = ",fjson[eachCol])
-        const stringElement = fjson[eachCol].toString()
+      const objectElement = (typeof fjson[eachCol] === "object")? JSON.stringify(fjson[eachCol]):fjson[eachCol]
+        const stringElement = objectElement.toString()
         const isStringPart = stringElement.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        console.log("stringElement = ",stringElement)
         console.log("isStringPart = ",isStringPart)
       
          if(isStringPart){
@@ -132,35 +148,33 @@ searchInput.addEventListener("input",async function(e){
     console.log("filteredJson",filteredJson)
 }
 
+/** Clean a container and fill with a new Element.
+ * 
+ * @param {HTMLElement} element 
+ * @param {string} receiverClassName 
+ */
+function cleanAndFillContainer(element,receiverClassName){
 
-function loadSelection(element){
-
-    
-   console.log()
     try{
-        console.log("loadSelection",element)
-        const tableContainer = document.getElementById("result-table")
-        const tableReceiver = document.getElementById("table-receiver")
-        console.log("tableContainer = ",tableContainer)
+
+        const tableReceiver = document.getElementById(receiverClassName)
         const table = document.getElementById("result-table")
-        console.log("table = ",table)
-    if(table !== null){
-        while (tableReceiver.firstChild) {
-            console.log("Is Child ? ",tableReceiver.firstChild)
+
+        if(table !== null){
+            while (tableReceiver.firstChild) {
             tableReceiver.removeChild(tableReceiver.lastChild);
-          }
-        console.log(`Replace Child de ${table} dans ${tableContainer} par nouvelle données.`)
-        
-      //  tableContainer.innerText = ""
-   }
-    else {
+            }
+        }
+
+        else {
         console.log("Pas de table existante, données initiale chargées.")
-      
-        
-    } tableReceiver.appendChild(element)
-        
-        
-}
+        } 
+    
+    tableReceiver.appendChild(element)
+
+    }
+
     catch {(element) => console.log("erreur" + element)}
 
 }
+
--- datacenter.js
+++ datacenter.js
@@ -1,6 +1,4 @@
 function placeHolder(type,number){
-
-
   return `https://jsonplaceholder.typicode.com/${type}/${number}`
 }
 
@@ -12,16 +10,16 @@ const types = [
   "todos",
   "users"]
 
-  async function loadData(types) {
-  try {
-    console.log(`Téléchargement de la page ${placeHolder(types,"")}.`)
-    const data = await fetch(placeHolder(types,""));
-    const jsonData = await data.json();
-    const jsonString = await JSON.stringify(jsonData)
-    await localStorage.setItem("freshJson",jsonString)
-   //  Modify DOM based on jsonData
+  async function loadData(types,localJsonName) {
+    try {
+      console.log(`Téléchargement de la page ${placeHolder(types,"")}.`)
+      const data = await fetch(placeHolder(types,""));
+      const jsonData = await data.json();
+      const jsonString = await JSON.stringify(jsonData)
+      await localStorage.setItem(localJsonName,jsonString)
     return jsonData
-  } catch (error) {
+    } 
+    catch (error) {
     return console.error('Error loading data:', error);
   }
 }
